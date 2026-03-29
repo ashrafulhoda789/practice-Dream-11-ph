774a316d97b81f72559f50b15abf28ce7629066e
ashraful-hoda <asrafulhoda789@gmail.com>
2026-03-29 12:42:41 +0600

    banner section completed

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import bannerBg from '../../assets/bg-shadow.png'
+
+const Banner = () => {
+    return (
+        <div
+        style={{
+            backgroundImage: `url(${bannerBg})`
+        }}
+         className='flex flex-col justify-center items-center my-10 border border-gray-300 p-5 md:p-20 container mx-auto rounded-2xl bg-cover bg-center space-y-5'>
+            
+            <div className='flex flex-col justify-center items-center space-y-2'>
+                <h1 className='text-xl md:text-2xl lg:text-[40px] font-bold'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
+                <h4 className='text-[16px] md:text-lg lg:text-2xl font-medium'>Beyond Boundaries Beyond Limits</h4>
+            </div>
+            <button className='btn bg-[#E7FE29] border border-black'>Claim Free Credit</button>
+        </div>
+    );
+};
+
+export default Banner;
